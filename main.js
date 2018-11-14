2a8326f5e8c1bc767c24bc1c23c0c9342fd16ba2
Stabmirz <amirza@techlaunch.io>
2018-11-14 00:16:16 -0500

    updated

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var newLogo = document.getElementById('logo');
 
 function changeLogo(){
-    newLogo.innerHTML=`<img src="./images/glasses.gif" style=''width= 240rem; height=128rem; alt="glasses-icon">`;
+    newLogo.innerHTML=`<img src="./images/glasses.gif" style='width:8rem; height:4rem;margin-top:2rem;margin-bottom:2rem;' alt="glasses-icon">`;
+    // var newIcon= document.getElementById('icon');
+    // newIcon.src= "./images/glasses.gif";
 }
 
 
+ 
