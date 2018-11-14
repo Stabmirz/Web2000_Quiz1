9cdc00d1e3c3dad42ec17d12e179dbe84de55a1d
Stabmirz <amirza@techlaunch.io>
2018-11-13 20:29:21 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var newLogo = document.getElementById('logo');
+
+function changeLogo(){
+    newLogo.innerHTML=`<img src="./images/glasses.gif" style=''width= 240rem; height=120rem; alt="glasses-icon">`;
+}
+
+
